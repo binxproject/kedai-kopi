d3b19a80443297a80e49250f3f5226fbd354191a
binx <iconiclampard99@gmail.com>
2024-11-30 10:00:17 +0700

    update code

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
 const navbarNav = document.querySelector('.navbar-nav');
 
-document.querySelector('#hamburger-menu').onclick = () => {
+document.querySelector('#hamburger-menu').onclick = (e) => {
     navbarNav.classList.toggle('active');
+    e.preventDefault();
+};
+
+const shoppingCart = document.querySelector('.shopping-cart');
+
+document.querySelector('#shopping-cart-button').onclick = (e) => {
+    shoppingCart.classList.toggle('active');
+    e.preventDefault();
 };
 
 // toggle search
@@ -17,14 +25,45 @@ document.querySelector('#search-button').onclick = (e) => {
 // klik diluar element
 const hm = document.querySelector('#hamburger-menu');
 const sb = document.querySelector('#search-button');
-
+const scb = document.querySelector('#shopping-cart-button');
 
 document.addEventListener('click', function(e) {
     if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
         navbarNav.classList.remove('active');
     }
 
-    else if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
         searchForm.classList.remove('active');
+    } 
+    
+    if (!scb.contains(e.target) && !shoppingCart.contains(e.target)) {
+        shoppingCart.classList.remove('active');
     }
 });
+
+// modal box
+const itemDetailModal = document.querySelector('#item-detail-modal');
+const itemDetailButtons = document.querySelectorAll('.item-detail-button');
+
+itemDetailButtons.forEach((btn) => {
+    btn.onclick = (e) => {
+        itemDetailModal.style.display = 'flex';
+        e.preventDefault();
+    };
+});
+
+
+
+
+// klik tombol close
+document.querySelector('.modal .close-icon').onclick = (e) => {
+    itemDetailModal.style.display = 'none';
+    e.preventDefault();
+}
+
+// klik diluar modal
+window.onclick = (e) => {
+    if (e.target === itemDetailModal) {
+        itemDetailModal.style.display = 'none';
+    }
+}
